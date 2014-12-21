0f1e9dffe173f724999c12dd4d6aec33a6c5572d
SacLo <ncchanh92@gmail.com>
2014-12-21 15:17:50 +0700

    Ver 1.1

---
+++ NMCNPM/Class/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMCNPM.Class
+{
+    public class User
+    {
+        private String _iUserName;
+
+        public String IUserName
+        {
+            get { return _iUserName; }
+            set { _iUserName = value; }
+        }
+        private String _iPassword;
+
+        public String IPassword
+        {
+            get { return _iPassword; }
+            set 
+            {
+                if (value != "")
+                {
+                    _iPassword = value;
+                }
+                //else
+                //{
+                //    _iPassword = "bb";
+                //} 
+                if (value==null)
+                {
+                    _iPassword = "";
+                }
+               // _iPassword = "asd";
+            }
+        }
+        private String _iMaLoai;
+
+        public String IMaLoai
+        {
+            get { return _iMaLoai; }
+            set { _iMaLoai = value; }
+        }
+
+    }
+}
